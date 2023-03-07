9ee3e7bba444ef4896649420f9dd5bc23fb1237a
Abdelrahman <kandiler@yahoo.com>
2023-03-06 19:38:42 -0500

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     // Optional: LinkedIn account URL
     linkedin: `https://www.linkedin.com/in/asolimandev/`,
     // Content of the About Me section
-    resume: `/Users/abdulrahmankandil/Documents/Github/asoliman.dev/asoliman.dev/`,
+    resume: `.`,
     // Content of the About Me section
     about: `Hello World Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.

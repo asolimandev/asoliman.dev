43a7ef847e325d559ce4627d1e195ba2374dade6
Abdelrahman <kandiler@yahoo.com>
2023-04-04 15:58:15 -0400

    testtest

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -66,13 +66,8 @@ const Header = ({ metadata = {}, noBlog = false }) => {
             </li>
           )}
           <li className={classes.item}>
-            <a className={classes.link} href={resumeFile} download="Abdelrahman_Soliman_Resume.pdf">
-              Resume <u>↓</u>
-            </a>
-          </li>
-          <li className={classes.item}>
             <a className={classes.link} href={contactFile} download="Abdelrahman_Soliman.vcf">
-              Add Contact
+              Contact
             </a>
           </li>
         </ul>

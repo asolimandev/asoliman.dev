33f5631850d46ba3c3cd8241f0bed0329770510c
Abdelrahman <kandiler@yahoo.com>
2023-04-04 17:59:07 -0400

    testerman

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,7 +3,6 @@ import get from 'lodash/get';
 import React from 'react';
 import resumeFile from '../../../r.pdf';
 import contactFile from '../../../c.pdf';
-import contactFile2 from '../../../c.vcf';
 import profileImg from '../../images/profile.jpg';
 const classes = {
   wrapper: 'block mb-6 md:flex',
@@ -81,7 +80,7 @@ const Header = ({ metadata = {}, noBlog = false }) => {
             </a>
           </li>
           <li className={classes.item}>
-            <a className={classes.link} href={contactFile2} type="text/vcard">
+            <a className={classes.link} href="../../../c.vcf" type="text/vcard">
               Contact
             </a>
           </li>

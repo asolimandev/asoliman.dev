21ad97320c68bc514a3fc59c03cb7e25e0129bb7
Abdelrahman <kandiler@yahoo.com>
2023-04-04 14:42:21 -0400

    test

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,8 @@
 import { Link } from 'gatsby';
 import get from 'lodash/get';
 import React from 'react';
-import resumeFile from '../../../r.pdf'
-import contactFile from '../../../c.vcf'
+// import resumeFile from '../../../r.pdf'
+// import contactFile from '../../../c.vcf'
 import profileImg from '../../images/profile.jpg';
 const classes = {
   wrapper: 'block mb-6 md:flex',
@@ -65,16 +65,6 @@ const Header = ({ metadata = {}, noBlog = false }) => {
               </Link>
             </li>
           )}
-          <li className={classes.item}>
-            <a className={classes.link} href={resumeFile} download="Abdelrahman_Soliman_Resume.pdf">
-              Resume <u>↓</u>
-            </a>
-          </li>
-          <li className={classes.item}>
-            <a className={classes.link} href={contactFile} download="Abdelrahman_Soliman.vcf">
-              Add Contact 👤+
-            </a>
-          </li>
         </ul>
       </div>
     </div>

16a023b612e974739a94dc98d79afca832b46d84
Abdelrahman <kandiler@yahoo.com>
2023-04-04 17:36:57 -0400

    joking

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,7 +2,7 @@ import { Link } from 'gatsby';
 import get from 'lodash/get';
 import React from 'react';
 import resumeFile from '../../../r.pdf';
-import contactFile from '../../../c.vcf';
+import contactFile from '../../../r.pdf';
 import profileImg from '../../images/profile.jpg';
 const classes = {
   wrapper: 'block mb-6 md:flex',
@@ -64,11 +64,11 @@ const Header = ({ metadata = {}, noBlog = false }) => {
               </Link>
             </li>
           )}
-            <li className={classes.item}>
-              <a className={classes.link} href={resumeFile} download="Abdelrahman_Soliman_Resume.pdf">
-                Resume <u>↓</u>
-              </a>
-            </li>
+          <li className={classes.item}>
+            <a className={classes.link} href={resumeFile} download="Abdelrahman_Soliman_Resume.pdf">
+              Resume <u>↓</u>
+            </a>
+          </li>
           <li className={classes.item}>
             <a className={classes.link} href={contactFile} download="Abdelrahman_Soliman.vcf">
               Contact
